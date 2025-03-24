243d9b20f7996f314acf241dd74f8679205f69be
pswbuild <pswbuild@oppo.com>
2025-03-24 21:01:04 +0800

    Synchronize code for OPPO CPH2579_15.0.0.500(EX01) CPH257922364_15.0.0.500(EX01) CPH257922368_15.0.0.500(EX01) Based on MTK release TAG:t-alps-release-v0.mp1.rc-V3

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -7902,6 +7902,8 @@ static int kswapd(void *p)
 	struct task_struct *tsk = current;
 	const struct cpumask *cpumask = cpumask_of_node(pgdat->node_id);
 
+        //set_user_nice(tsk, -15);
+
 	if (!cpumask_empty(cpumask))
 		set_cpus_allowed_ptr(tsk, cpumask);
 
